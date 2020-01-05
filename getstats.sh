32ee49a1971962b538588d2ea6486248d7c3bf9a
Jiri (George) Lebl <jiri.lebl@gmail.com>
2020-01-04 19:11:51 -0600

    Some work

--- getstats.sh
+++ getstats.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 echo
-echo "Chapters (including intro):   \t"`grep '^[\]chapter\**{.*label{ch:' *.tex | wc -l`
-echo "Appendices:                   \t"`grep '^[\]chapter{.*label{ap:' *.tex | wc -l`
-echo "Sections:                     \t"`grep '^[\]section\**{' *.tex | wc -l`
+echo "Chapters (including intro):    \t"`grep '^[\]chapter\**{.*label{ch:' ca.tex | wc -l`
+echo "Appendices:                    \t"`grep '^[\]chapter{.*label{ap:' ca.tex | wc -l`
+echo "Sections:                      \t"`grep '^[\]section\**{' ca.tex | wc -l`
+echo "Sections (excluding appendix): \t"`cat ca.tex | sed '/^[\]appendix/q' | grep '^[\]section\**{' | wc -l`
 echo
-echo "Figures:                      \t"`grep '^[\]begin{myfig' *.tex | wc -l`
+echo "Figures:                       \t"`grep '^[\]begin{myfig' ca.tex | wc -l`
+echo "Figures (excluding appendix):  \t"`cat ca.tex | sed '/^[\]appendix/q' | grep '^[\]begin{myfig' | wc -l`
 echo
-echo "Exercises:                    \t"`grep '^[\]begin{exercise' *.tex | wc -l`
+echo "Exercises:                     \t"`grep '^[\]begin{exercise' ca.tex | wc -l`
+echo "Exercises (excluding appendix):\t"`cat ca.tex | sed '/^[\]appendix/q' | grep '^[\]begin{exercise' | wc -l`
 
 echo
