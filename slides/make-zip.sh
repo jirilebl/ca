677b9f759ef30ed639a893eb205728abade0f31d
Jiri (George) Lebl <jiri.lebl@gmail.com>
2023-01-12 19:20:08 -0600

    Improvements to style

--- slides/make-zip.sh
+++ slides/make-zip.sh
@@ -5,17 +5,7 @@ for n in *.tex ; do
 	echo =============================
 	p=`basename $n .tex`".pdf"
 	echo $n '-->' $p
-	# Just always rebuild rubber is smart
 	rubber -d $n
-	#if test '!' -e $p ; then
-	#	echo Missing, will rebuild
-	#	rubber -d $n
-	#elif test $n -nt $p ; then
-	#	echo Old, will rebuild
-	#	rubber -d $n
-	#else
-	#	echo No need to rebuild
-	#fi
 done
 
 echo =============================
