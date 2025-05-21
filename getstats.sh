0c89feac1a1fe9c352e806d97e30b0b79f881c5e
Jiri (George) Lebl <jiri.lebl@gmail.com>
2025-05-20 22:50:06 -0500

    Fixup getstats.sh

--- getstats.sh
+++ getstats.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 echo
-echo "Chapters (including intro):    \t"`grep '^[\]chapter\**{.*label{ch:' ca.tex | wc -l`
-echo "Appendices:                    \t"`grep '^[\]chapter{.*label{ap:' ca.tex | wc -l`
-echo "Sections:                      \t"`grep '^[\]section\**{' ca.tex | wc -l`
-echo "Sections (excluding appendix): \t"`cat ca.tex | sed '/^[\]appendix/q' | grep '^[\]section\**{' | wc -l`
+echo -e "Chapters (including intro):    \t"`grep '^[\]chapter\**{.*label{ch:' ca.tex | wc -l`
+echo -e "Appendices:                    \t"`grep '^[\]chapter{.*label{ap:' ca.tex | wc -l`
+echo -e "Sections:                      \t"`grep '^[\]section\**{' ca.tex | wc -l`
+echo -e "Sections (excluding appendix): \t"`cat ca.tex | sed '/^[\]appendix/q' | grep '^[\]section\**{' | wc -l`
 echo
-echo "Figures:                       \t"`grep '^[\]begin{myfig' ca.tex | wc -l`
-echo "Figures (excluding appendix):  \t"`cat ca.tex | sed '/^[\]appendix/q' | grep '^[\]begin{myfig' | wc -l`
+echo -e "Figures:                       \t"`grep '^[\]begin{myfig' ca.tex | wc -l`
+echo -e "Figures (excluding appendix):  \t"`cat ca.tex | sed '/^[\]appendix/q' | grep '^[\]begin{myfig' | wc -l`
 echo
-echo "Exercises:                     \t"`grep '^[\]begin{exercise' ca.tex | wc -l`
-echo "Exercises (excluding appendix):\t"`cat ca.tex | sed '/^[\]appendix/q' | grep '^[\]begin{exercise' | wc -l`
+echo -e "Exercises:                     \t"`grep '^[\]begin{exercise' ca.tex | wc -l`
+echo -e "Exercises (excluding appendix):\t"`cat ca.tex | sed '/^[\]appendix/q' | grep '^[\]begin{exercise' | wc -l`
 echo
-echo "Footnotes:                     \t"`grep '[\]footnote{' ca.tex | wc -l`
-echo "Footnotes (excluding appendix):\t"`cat ca.tex | sed '/^[\]appendix/q' | grep '[\]footnote{' | wc -l`
+echo -e "Footnotes:                     \t"`grep '[\]footnote{' ca.tex | wc -l`
+echo -e "Footnotes (excluding appendix):\t"`cat ca.tex | sed '/^[\]appendix/q' | grep '[\]footnote{' | wc -l`
 
 echo
